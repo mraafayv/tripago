5e217e3378acac855dc618f119a4bafb0e01ca8a
mraafayv <raafayvohra.mrv@gmail.com>
2022-11-29 11:45:59 +0500

    iitial commit

---
+++ src/components/TripList.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
+
+//Styles
+import "./TripList.css";
+
+export default function TripList() {
+  // const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
+
+  const { data: trips, isPending } = useFetch(url);
+  return (
+    <div>
+      <h2 id="heading">Trip List</h2>
+      {isPending && <div>Loading Trips...</div>}
+      <div>
+        {trips && trips.map((trip) => {
+          return (
+            <div className="trip-card" key={trip.id}>
+              <h2>{trip.title}</h2>
+              <h4 className="price">{trip.price}</h4>
+              <p className="loc">{trip.loc}</p>
+            </div>
+          );
+        })}
+        <div className="filters">
+          <button
+            onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+          >
+            European Trips
+          </button>
+          <button
+            onClick={() => setUrl("http://localhost:3000/trips?loc=america")}
+          >
+            US Trips{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  useEffect(()=> {
+    const fetchData = async () => {
+        setIsPending(true);
+        const response = await fetch(url);
+        const json = await response.json();
+        
+        setIsPending(false);
+        setData(json);
+    }
+
+    fetchData();
+  }, [url])
+
+
+  return { data, isPending }
+};
